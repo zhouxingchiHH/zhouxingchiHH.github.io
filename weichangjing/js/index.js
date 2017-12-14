17027bbb9b9072969a92447e27fc0d3d87a1f3c2
那么好 <910047885@qq.com>
2017-12-14 10:03:46 +0800

    swiper

---
+++ weichangjing/js/index.js
@@ -0,0 +1,45 @@
+ var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+     
+     navigation:{
+         nextEl:'.swiper-button-next',
+         
+     },
+     on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    },
+     
+    });
+
+    
+
+var music=document.querySelector('.music');
+var mus=document.querySelector('.mus');
+var gif=document.querySelector('.gif');
+var audio=document.querySelector('audio');
+var flag=1;
+music.onclick=function(){
+    if(flag==1){
+        mus.style.animation="none";
+        gif.display="none";
+        audio.pause();
+        flag=0;
+    }else{
+        mus.style.animation="music 1.8s  linear infinite";
+        gif.display="block";
+        audio.play();
+        flag=1;
+    }
+}
+
+
